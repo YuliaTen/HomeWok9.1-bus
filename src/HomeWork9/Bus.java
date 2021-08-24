62cd70509640588cbae77e7c93c224d85821b766
Yulia_Tenchurina <teenager111@yandex.ru>
2021-08-24 09:11:21 +0300

    Задача решена с помощью учителя. Создали стрим подсчитывающих кол-во люде оставшихся в автобусе

--- src/HomeWork9/Bus.java
+++ src/HomeWork9/Bus.java
@@ -11,16 +11,19 @@ public class Bus {
         System.out.println(" Введите кол-во остановок в пределах от 2х до 10");
         try{
             countBusStation = scanner.nextInt();
+            if (countBusStation<2 || countBusStation>10){
+                System.err.println("Вы ввели значение не из диапазона");
+                System.exit(0);
+            }
         }catch (Exception e){
             System.err.println("Вы ввели недопустимое значение "+ e);
             System.exit(0);
         }
         ArrayList<BusStaton> busStations;
         busStations = createBusStation(countBusStation);
-        System.out.println("");
-        int ostatok=0;
-        busStations.stream().forEach(station ->
-            System.out.println(ostatok = ostatok+station.inputPeople-station.outputPeople));
+        int ostatok = busStations.stream().mapToInt(s -> s.inputPeople - s.outputPeople).sum();
+        System.out.println(String.format
+          ("Количество человек оставшихся в автобусе при приближении к конечной станции - %d",ostatok));
 
 
     }
